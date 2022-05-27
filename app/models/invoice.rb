84edf1596a4e66b953e306fa1eebac1fd6ea3a33
Scott Sullivan <scottsullivanltd@gmail.com>
2022-05-27 01:42:40 -0600

    Refactor: model relationships and tests for invoice and merchant

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,8 +2,8 @@ class Invoice < ApplicationRecord
   validates :status, presence: true
 
   belongs_to :customer
+  belongs_to :merchant
   has_many :transactions
   has_many :invoice_items
   has_many :items, through: :invoice_items
-  has_many :merchants, through: :items
 end
